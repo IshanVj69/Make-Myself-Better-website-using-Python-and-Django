56be4b29bb7533fa77783ba3d94a7a297c3928e3
Ishan Vijay <107020804+IshanVj69@users.noreply.github.com>
2022-12-24 16:08:26 +0530

    forth commit

---
+++ static/js/like.js
@@ -0,0 +1,6 @@
+
+let btnDanger = document.querySelector('#danger');
+let btnSuccess = document.querySelector('#success');
+
+btnDanger.addEventListener('click', () => btnDanger.style.backgroundColor = '#c9302c')
+btnSuccess.addEventListener('click', () => btnSuccess.style.backgroundColor = '#4cae4c')
